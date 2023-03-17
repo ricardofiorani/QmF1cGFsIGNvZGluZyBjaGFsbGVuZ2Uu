c8684567b39886dc48d4504c924fcd2d950f97bd
Ricardo Fiorani <ricardo.fiorani@gmail.com>
2023-03-17 11:47:45 +0100

    Added implementation of the get form structure endpoint

--- src/adapter/in/express/forms/structure/getUserFormStructureHandler.ts
+++ src/adapter/in/express/forms/structure/getUserFormStructureHandler.ts
@@ -1,5 +1,14 @@
 import {Response, Request} from "express-serve-static-core";
+import FormStructureGetterAdapter from "../../../../out/hardcoded/FormStructureGetterAdapter";
+import GetFormStructureUseCase from "../../../../../application/useCase/getFormStructureUseCase";
 
-export const getUserFormStructureHandler = (req: Request, res: Response) => {
-    res.send("pong");
+export const getUserFormStructureHandler = async (req: Request, res: Response) => {
+    try {
+        const formStructureGetterAdapter = new FormStructureGetterAdapter();
+        const useCase = new GetFormStructureUseCase(formStructureGetterAdapter);
+        const formStructure = await useCase.getFormStructure();
+        res.send(formStructure);
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
 }
