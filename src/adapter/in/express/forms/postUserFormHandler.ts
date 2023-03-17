c2a277e9c959cdd9b6fd57415e520ceea2dc352b
Ricardo Fiorani <ricardo.fiorani@gmail.com>
2023-03-17 13:27:12 +0100

    Added JSON parser for express, and wired final parts of the application

--- src/adapter/in/express/forms/postUserFormHandler.ts
+++ src/adapter/in/express/forms/postUserFormHandler.ts
@@ -1,5 +1,20 @@
 import {Request, Response} from "express-serve-static-core";
+import UserFormValidator from "../../../out/hardcoded/UserFormValidator";
+import InsertUserFormUseCase from "../../../../application/useCase/insertUserForm";
+import UserFormRepository from "../../../out/database/UserFormRepository";
+import {UserForm} from "../../../../domain/forms";
 
-export const postUserFormHandler = (req: Request, res: Response) => {
-    res.send("pong");
+export const postUserFormHandler = async (req: Request, res: Response) => {
+    try {
+        const userForm = await req.body.data as UserForm;
+        const validator = new UserFormValidator();
+        const repository = new UserFormRepository();
+        const useCase = new InsertUserFormUseCase(repository, validator);
+        const result = await useCase.insertUserForm(userForm);
+        //In here we would add some logic for this layer, like, if it fails validation its the users fault the return 400
+        // For the sake of simplicity I'm just returning the result
+        res.send(result);
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 import {pingHandler} from "./adapter/in/express/pingHandler";
 import {getUserFormStructureHandler} from "./adapter/in/express/forms/structure/getUserFormStructureHandler";
 import {postUserFormHandler} from "./adapter/in/express/forms/postUserFormHandler";
+import bodyParser from "body-parser";
 
 export const server = express();
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
 
 server.get("/", pingHandler);
 server.get("/forms/structure/userForm", getUserFormStructureHandler);
